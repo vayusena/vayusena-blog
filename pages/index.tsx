b8c35a94cc9da34b0fdab3054ca1dbc4008b9c1a
ARGF0RCE <arg132oo2@gmail.com>
2022-10-23 12:57:24 +0530

    Edit_0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,32 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
-
-const posts = [
-  { title: 'OpenDMS', excerpt: 'Learn OpenDMS' },
-  { title: 'OpenDMS', excerpt: 'Learn OpenDMS Test' },
-];
-
-const Home: NextPage = () => {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>VayuSena Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols12 gap-12'>4
-        <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
-
+      <FeaturedPosts />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
+}
